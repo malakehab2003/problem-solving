a22b3799df2718bac4e158f753b026d95620bb6c
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2025-07-17 16:56:39 +0300

    add L. Max Subarray.py file

---
+++ assuit_sheet/Sheet #3/L. Max Subarray.py
@@ -0,0 +1,23 @@
+tests = int(input())
+
+for _ in range(tests):
+    no_of_nums = int(input())
+
+    nums = list(map(int, input().split()))
+
+    i = 0
+    j = 1
+    max_int = []
+    while j <= no_of_nums:
+        while i + j <= no_of_nums:
+            sub_array = nums[i:i + j]
+            max_int.append(max(sub_array))
+            i += 1
+        i = 0
+        j += 1
+
+    for i in range(len(max_int)):
+        if i == len(max_int) -1:
+            print(max_int[i])
+            break
+        print(max_int[i], end=" ")
