c33f864d109bf90e69b1124027becdd4871405e0
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2025-07-26 19:27:03 +0300

    fix Y. Range sum query.py file

--- assuit_sheet/Sheet #3/Y. Range sum query.py
+++ assuit_sheet/Sheet #3/Y. Range sum query.py
@@ -2,11 +2,10 @@ n, q = map(int, input().split())
 
 a = list(map(int, input().split()))
 
+prefix = [0] * (n + 1)
+for i in range(n):
+    prefix[i + 1] = prefix[i] + a[i]
+
 for _ in range(q):
     l, r = map(int, input().split())
-
-    sum = 0
-    for i in range(l - 1, r):
-        sum += a[i]
-
-    print(sum)
+    print(prefix[r] - prefix[l - 1])
